56a4d87c74381a0c07f31a45134404c4ed0dcd6e
Jeevesh Srivastava <jeeveshsrivastava18@gmail.com>
2024-08-16 16:00:00 +0530

    Fix duplicate verify_firmware_integrity, add missing hash.c to CMakeLists

--- projects/secure-boot-ota/bootloader/src/verify.c
+++ projects/secure-boot-ota/bootloader/src/verify.c
@@ -26,31 +26,5 @@ int verify_firmware_signature(const uint8_t *firmware, size_t size,
     return 0;
 }
 
-int verify_firmware_integrity(const uint8_t *firmware, size_t size)
-{
-    uint8_t calculated_hmac[HMAC_SIZE];
-    uint8_t stored_hmac[HMAC_SIZE];
-    extern uint8_t hmac_key[];
-    
-    if (!firmware || size == 0) {
-        return -1;
-    }
-    
-    /* Calculate HMAC */
-    if (crypto_hmac_sha256(firmware, size - HMAC_SIZE,
-                          hmac_key, 32,
-                          calculated_hmac) != CRYPTO_SUCCESS) {
-        return -1;
-    }
-    
-    /* Get stored HMAC from end of firmware */
-    memcpy(stored_hmac, firmware + size - HMAC_SIZE, HMAC_SIZE);
-    
-    /* Compare */
-    if (memcmp(calculated_hmac, stored_hmac, HMAC_SIZE) != 0) {
-        return -1;
-    }
-    
-    return 0;
-}
+/* verify_firmware_integrity() is implemented in integrity.c */
 
